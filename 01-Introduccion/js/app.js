04ecf1f42f225a480c4b66a584321c39c5089758
Eliseo <elicarortix58@gmail.com>
2022-12-19 18:35:12 -0600

    subiendo el proyecto,primer commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,28 @@
+/*const nombre = prompt('¿cual es tu nombre?')
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScritp Moderno`;*/
+
+//console.error('algo salio mal');
+
+/*console.time('hola');
+
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+console.warn('esto no esta permitido');
+
+console.timeEnd('hola');*/
+
+/*const obj = {
+    nombre: "eliseo",
+    edad: 21
+};*/
+
+console.log('hola');
+console.log('adios');
+
+
---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,24 @@
+var producto = "monitor de 24 pulgadas"
+console.log(producto)
+
+//las variables se pueden reasignar
+producto = 'monitor de 19 pulgadas'
+console.log(producto)
+
+//javascript es un lenguaje tipo dinamico, no se especifica tipo de dato
+producto = 20
+console.log(producto)
+
+
+
+//se pueden inicializar sin valor y asignarle algo despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//inicializar multiples variables
+var categoria = 'computadoras',
+    marca = 'marca famosa',
+    calificacion = 5;
+
